278eebdf589150c59ae0ec118622212f92a55172
Ingvar22 <152574617+Ingvar22@users.noreply.github.com>
2024-04-07 15:28:57 +0700

    Delete 1 directory

--- 1/create_contact.h
+++
@@ -1,8 +0,0 @@
-#ifndef CREATE_CONTACT_H
-#define CREATE_CONTACT_H
-
-#include "contact_node.h"
-
-ContactNode* createContact(char* firstName, char* lastName, char* jobTitle, char* workplace, char* phone, char* email, char* socialMedia, char* messenger);
-
-#endif
