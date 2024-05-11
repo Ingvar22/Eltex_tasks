36ee9bc092b500a0c88a173fa6743cca3614a5a5
Ingvar22 <152574617+Ingvar22@users.noreply.github.com>
2024-05-11 21:16:52 +0700

    Delete Module3/5 directory

--- Module3/5/display_menu.h
+++
@@ -1,6 +0,0 @@
-#ifndef DISPLAY_MENU_H
-#define DISPLAY_MENU_H
-
-void displayMenu();
-
-#endif
--- Module3/5/full_name.h
+++
@@ -1,9 +0,0 @@
-#ifndef FULL_NAME_H
-#define FULL_NAME_H
-
-typedef struct {
-    char firstName[50];
-    char lastName[50];
-} FullName;
-
-#endif
